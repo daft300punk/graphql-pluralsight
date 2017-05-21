7d4a915925646ecd604b098f8a56415656998823
Abhilash Singh <daft300punk@gmail.com>
2017-05-21 23:54:09 +0530

    Cyclic module dependency problem

--- database/pgdb.js
+++ database/pgdb.js
@@ -2,13 +2,22 @@ const humps = require('humps');
 
 module.exports = pgPool => {
   return {
-    getUser(apiKey) {
+    getUserByApiKey(apiKey) {
       return pgPool.query(`
         select * from users
         where api_key = $1
       `, [apiKey]).then(res => {
         return humps.camelizeKeys(res.rows[0]);
       })
+    },
+
+    getUserById(userId) {
+      return pgPool.query(`
+        select * from users
+        where id = $1
+      `, [userId]).then(res => {
+        return humps.camelizeKeys(res.rows[0]);
+      });
     },
 
     getContests(user) {
@@ -18,6 +27,15 @@ module.exports = pgPool => {
       `, [user.id]).then(res => {
         return humps.camelizeKeys(res.rows);
       })
+    },
+
+    getNames(contest) {
+      return pgPool.query(`
+        select * from names
+        where contest_id = $1
+      `, [contest.id]).then(res => {
+        return humps.camelizeKeys(res.rows);
+      });
     }
   };
 };
--- schema/types/contest.js
+++ schema/types/contest.js
@@ -2,10 +2,13 @@ const {
   GraphQLObjectType,
   GraphQLNonNull,
   GraphQLString,
-  GraphQLID
+  GraphQLID,
+  GraphQLList
 } = require('graphql');
 
 const ContestStatusType = require('./contest-status');
+const pgdb = require('../../database/pgdb');
+const NameType = require('./name');
 
 module.exports = new GraphQLObjectType({
   name: 'ContestStatusType',
@@ -16,6 +19,12 @@ module.exports = new GraphQLObjectType({
     title: { type: new GraphQLNonNull(GraphQLString) },
     description: { type: GraphQLString },
     status: { type: new GraphQLNonNull(ContestStatusType) },
-    createAt: { type: new GraphQLNonNull(GraphQLString) }
+    createAt: { type: new GraphQLNonNull(GraphQLString) },
+    names: {
+      type: new GraphQLList(NameType),
+      resolve(obj, args, { pgPool }) {
+        return pgdb(pgPool).getNames(obj);
+      }
+    }
   }
 });
